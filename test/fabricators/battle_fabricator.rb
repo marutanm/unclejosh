3f9baee3bc3153548cbb6b2f1c22540ca0d9380e
marutanm <marutanm@gmail.com>
2013-02-07 23:03:40 +0900

    improve battle fabricator

--- test/fabricators/battle_fabricator.rb
+++ test/fabricators/battle_fabricator.rb
@@ -1,5 +1,8 @@
 Fabricator(:battle) do
   masters { [ Fabricate(:hero) ] }
   challengers { [ Fabricate(:hero) ] }
+
+  master_attacks { [] }
+  challenger_attack { [] }
 end
 
--- test/models/battle_test.rb
+++ test/models/battle_test.rb
@@ -7,11 +7,23 @@ describe "Battle Model" do
   end
 
   describe 'accessor' do
-    subject { Fabricate.build(:battle) }
+    subject do
+      Fabricate(:battle) do
+          after_create do |battle|
+            10.times do
+              battle.master_attacks << Fabricate.build(:turn)
+              battle.challenger_attacks << Fabricate.build(:turn)
+            end
+          end
+      end
+    end
+
     specify do
       subject.master.wont_be_nil
       subject.challenger.wont_be_nil
       subject.winner.must_be_nil
+      subject.master_attacks.count.must_equal 10
+      subject.challenger_attacks.count.must_equal 10
     end
   end
 end
