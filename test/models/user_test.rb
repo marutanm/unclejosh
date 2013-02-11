42d1649970833276e34b5bdc11feb7e0b139b993
marutanm <marutanm@gmail.com>
2013-02-11 09:23:19 +0900

    Add user_fabricator

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
 
+describe "Fabricate User" do
+  let(:user) { Fabricate(:user) }
+  specify do
+    user.wont_be_nil
+    user.name.must_be_kind_of String
+  end
+end
+
 describe "User Model" do
   it 'can construct a new instance' do
     @user = User.new
