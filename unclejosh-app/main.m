9ea61fb864c16b3a877de130eab13d71f1c9b15f
marutanm <marutanm@gmail.com>
2013-02-13 21:26:20 +0900

    Initial Commit

---
+++ unclejosh-app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  unclejosh-app
+//
+//  Created by Ryo Fujita on 2/13/2013.
+//  Copyright (c) 2013 Ryo Fujita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UJAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UJAppDelegate class]));
+    }
+}
