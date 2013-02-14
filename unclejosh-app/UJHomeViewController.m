26ecb9c020fbb86736b8274cc84c46579e260c71
marutanm <marutanm@gmail.com>
2013-02-15 01:09:44 +0900

    Create name form and send button

--- unclejosh-app/UJHomeViewController.m
+++ unclejosh-app/UJHomeViewController.m
@@ -11,7 +11,7 @@
 #import "UJHttpClient.h"
 
 @interface UJHomeViewController ()
-
+@property(nonatomic,strong) UIButton *button;
 @end
 
 @implementation UJHomeViewController
@@ -28,8 +28,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+}
 
+- (void)viewDidAppear:(BOOL)animated
+{
     NIDPRINT(@"%@", [UJHttpClient isValid] ? @"YES" : @"NO");
+    if (![UJHttpClient isValid]) {
+        UIViewController *viewController = [[UIViewController alloc] init];
+        UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(10, 10, 300, 20)];
+        textField.placeholder = @"name";
+        [textField becomeFirstResponder];
+        [viewController.view addSubview:textField];
+
+        self.button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [self.button setTitle:@"SEND" forState:UIControlStateNormal];
+        self.button.frame = CGRectMake(110, 40, 100, 20);
+        [self.button addTarget:self action:@selector(onSend:) forControlEvents:UIControlEventTouchUpInside];
+        [viewController.view addSubview:self.button];
+
+        [self presentViewController:viewController animated:YES completion:nil];
+    }
+}
+
+- (void)onSend:(id)sender
+{
+    NIDPRINTMETHODNAME();
 }
 
 - (void)didReceiveMemoryWarning
