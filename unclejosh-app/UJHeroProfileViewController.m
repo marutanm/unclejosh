133b72f17230c628a8a3d172f63d4950f042cf68
marutanm <marutanm@gmail.com>
2013-02-19 23:44:39 +0900

    Show result button on finish challenge ranking

--- unclejosh-app/UJHeroProfileViewController.m
+++ unclejosh-app/UJHeroProfileViewController.m
@@ -19,6 +19,7 @@
 
 @property UIButton *challengebutton;
 @property UILabel *resultLabel;
+@property UIButton *resultButton;
 
 @end
 
@@ -38,7 +39,14 @@
         _challengebutton.frame = CGRectMake(10, 40, 140, 20);
         _challengebutton.hidden = YES;
         [_challengebutton addTarget:self action:@selector(challengeRanking:) forControlEvents:UIControlEventTouchUpInside];
+
         _resultLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 70, 140, 20)];
+
+        _resultButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [_resultButton setTitle:NSLocalizedString(@"RESULT", @"Label on show challenge result button") forState:UIControlStateNormal];
+        _resultButton.frame = CGRectMake(10, 70, 140, 20);
+        _resultButton.hidden = YES;
+        [_resultButton addTarget:self action:@selector(showResults) forControlEvents:UIControlEventTouchUpInside];
     }
     return self;
 }
@@ -56,6 +64,7 @@
 
     [self.view addSubview:_challengebutton];
     [self.view addSubview:_resultLabel];
+    [self.view addSubview:_resultButton];
 }
 
 - (void)didReceiveMemoryWarning
@@ -73,9 +82,15 @@
     [[UJHttpClient sharedClient] postPath:@"rankings" parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
         NIDPRINT(@"%@", responseObject);
         _resultLabel.text = [NSString stringWithFormat:NSLocalizedString(@"win:%@ ranking:%@", @"Result of challenge ranking"), [responseObject objectForKey:@"initial_win"], [responseObject objectForKey:@"rank"]];
+        _resultButton.hidden = NO;
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NIDPRINT(@"%@", error);
     }];
+}
+
+- (void)showResults;
+{
+    NIDPRINTMETHODNAME();
 }
 
 - (void)setHeroInfo:(id)info
