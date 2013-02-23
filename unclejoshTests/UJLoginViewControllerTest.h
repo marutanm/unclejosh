7ee5254e93a8010e7ce870e010998b21ae379274
marutanm <marutanm@gmail.com>
2013-02-23 10:07:27 +0900

    Add test of login view

---
+++ unclejoshTests/UJLoginViewControllerTest.h
@@ -0,0 +1,13 @@
+//
+//  UJLoginViewControllerTest.h
+//  unclejosh-app
+//
+//  Created by Ryo Fujita on 2/23/2013.
+//  Copyright (c) 2013 Ryo Fujita. All rights reserved.
+//
+
+#import "GHViewTestCase.h"
+
+@interface UJLoginViewControllerTest : GHViewTestCase
+
+@end
