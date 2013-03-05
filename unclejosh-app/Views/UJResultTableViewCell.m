f8af7fbcce6d4178ae952a304e0722b0b6cc743a
marutanm <marutanm@gmail.com>
2013-03-05 21:21:18 +0900

    Improve result tableview cell

--- unclejosh-app/Views/UJResultTableViewCell.m
+++ unclejosh-app/Views/UJResultTableViewCell.m
@@ -26,6 +26,10 @@
         _resultLabel.backgroundColor = [UIColor clearColor];
         _resultLabel.translatesAutoresizingMaskIntoConstraints = NO;
         _resultLabel.shadowColor = ZURUI_LIGHT_COLOR;
+        _resultLabel.textAlignment = NSTextAlignmentCenter;
+        _resultLabel.font = [UIFont boldSystemFontOfSize:18];
+        _resultLabel.layer.borderWidth = 4.0;
+        _resultLabel.layer.cornerRadius = 8;
         [self.contentView addSubview:_resultLabel];
 
         _nameLabel = [[UILabel alloc] init];
@@ -35,9 +39,9 @@
         [self.contentView addSubview:_nameLabel];
 
         NSMutableArray *constraints = [NSMutableArray array];
-        [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"|-[_resultLabel]-[_nameLabel]" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_resultLabel, _nameLabel)]];
+        [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"|-4-[_resultLabel(==60)]-[_nameLabel]" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_resultLabel, _nameLabel)]];
+        [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-4-[_resultLabel]-4-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_resultLabel)]];
         [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_nameLabel]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_nameLabel)]];
-        [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_resultLabel]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_resultLabel)]];
         [self.contentView addConstraints:constraints];
 
         self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
@@ -59,14 +63,17 @@
     if (![result objectForKey:@"winner_id"]) {
         _resultLabel.text = NSLocalizedString(@"DRAW", @"label of result table: draw");
         _resultLabel.textColor = [UIColor greenColor];
+        _resultLabel.layer.borderColor  = [UIColor greenColor].CGColor;
 
     } else if ([[result objectForKey:@"winner_id"] isEqualToString:[[result objectForKey:@"master"] objectForKey:@"id"]]) {
         _resultLabel.text = NSLocalizedString(@"WIN", @"label of result table: win");
         _resultLabel.textColor = [UIColor redColor];
+        _resultLabel.layer.borderColor  = [UIColor redColor].CGColor;
 
     } else {
         _resultLabel.text = NSLocalizedString(@"LOSE", @"label of result table: lose");
         _resultLabel.textColor = [UIColor blueColor];
+        _resultLabel.layer.borderColor  = [UIColor blueColor].CGColor;
     }
 
     _nameLabel.text = [[result objectForKey:@"master"] objectForKey:@"name"];
