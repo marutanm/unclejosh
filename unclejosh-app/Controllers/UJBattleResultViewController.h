ee2b9e96cb9c9dc1d67f5e08dce6cfa5e5c6ff70
marutanm <marutanm@gmail.com>
2013-03-07 21:52:55 +0900

    Show tableview on battle result

--- unclejosh-app/Controllers/UJBattleResultViewController.h
+++ unclejosh-app/Controllers/UJBattleResultViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface UJBattleResultViewController : UIViewController
+@interface UJBattleResultViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, copy) NSString* battleId;
 
--- unclejosh-app/Controllers/UJBattleResultViewController.m
+++ unclejosh-app/Controllers/UJBattleResultViewController.m
@@ -11,6 +11,12 @@
 
 @interface UJBattleResultViewController ()
 
+@property (nonatomic) UITableView *tableView;
+
+@property (nonatomic) NSArray *turns;
+@property (nonatomic) NSDictionary *challenger;
+@property (nonatomic) NSDictionary *master;
+
 @end
 
 @implementation UJBattleResultViewController
@@ -19,7 +25,9 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        _tableView = [[UITableView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame] style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
     }
     return self;
 }
@@ -28,19 +36,58 @@
 {
     [super viewDidLoad];
 
+    [self.view addSubview:_tableView];
+
     NSString *path = [NSString stringWithFormat:@"battles/%@", _battleId];
     [[UJHttpClient sharedClient] getPath:path parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
         NIDPRINT(@"%@", responseObject);
+        _turns = [responseObject objectForKey:@"turns"];
+        _challenger = [responseObject objectForKey:@"challenger"];
+        _master = [responseObject objectForKey:@"master"];
+
+        [_tableView reloadData];
 
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NIDPRINT(@"%@", error);
     }];
+
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _turns.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+
+    NSDictionary *turn = [NSDictionary dictionaryWithDictionary:[_turns objectAtIndex:indexPath.row]];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@:%@", [turn objectForKey:@"owner"], [turn objectForKey:@"damage"]];
+
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
