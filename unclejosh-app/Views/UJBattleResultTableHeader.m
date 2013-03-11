b4a1565fdc85fa0757de65a14c33534ce8b647b0
marutanm <marutanm@gmail.com>
2013-03-11 13:17:00 +0900

    Layout life gauge

--- unclejosh-app/Views/UJBattleResultTableHeader.m
+++ unclejosh-app/Views/UJBattleResultTableHeader.m
@@ -22,18 +22,31 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
+        self.backgroundColor = ZURUI_LIGHT_COLOR;
+
         _leftGauge = [[DPMeterView alloc] init];
         _leftGauge.meterType = DPMeterTypeLinearHorizontal;
-        _leftGauge.progressTintColor = [UIColor redColor];
+        _leftGauge.progressTintColor = LIFE_COLOR;
+        _leftGauge.trackTintColor = [UIColor redColor];
         _leftGauge.translatesAutoresizingMaskIntoConstraints = NO;
         [self addSubview:_leftGauge];
 
         _rightGauge = [[DPMeterView alloc] init];
-        _rightGauge.meterType = DPMeterTypeLinearHorizontal;
-        _rightGauge.progressTintColor = [UIColor redColor];
+        _rightGauge.gradientOrientationAngle = M_PI;
+        _rightGauge.progressTintColor = LIFE_COLOR;
+        _rightGauge.trackTintColor = [UIColor redColor];
         _rightGauge.translatesAutoresizingMaskIntoConstraints = NO;
         [self addSubview:_rightGauge];
 
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_leftGauge(==_rightGauge)][_rightGauge]|"
+                                                                     options:0
+                                                                     metrics:nil
+                                                                       views:NSDictionaryOfVariableBindings(_leftGauge, _rightGauge)]];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_leftGauge(==_rightGauge)]|"
+                                                                     options:0
+                                                                     metrics:nil
+                                                                       views:NSDictionaryOfVariableBindings(_leftGauge, _rightGauge)]];
+        [self addConstraint:[NSLayoutConstraint constraintWithItem:_rightGauge attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:_leftGauge attribute:NSLayoutAttributeTop multiplier:1.0 constant:0]];
     }
     return self;
 }
@@ -41,6 +54,8 @@
 - (void)setLifesLeft:(NSInteger)left right:(NSInteger)right;
 {
     NIDPRINT(@"%d:%d", left, right);
+    [_leftGauge setProgress:left / 1000.0];
+    [_rightGauge setProgress:right/ 1000.0];
 }
 
 @end
